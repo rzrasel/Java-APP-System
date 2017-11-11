3408dedc8ff1ce94675cf82b1b2c58fd8452e3f3
Developer <developer@Shurjo.local>
2017-11-11 13:52:25 +0600

    Normal Commit

--- src/com/rz/DbMigration/CreateTableAPPProject.java
+++ src/com/rz/DbMigration/CreateTableAPPProject.java
@@ -17,13 +17,16 @@ public class CreateTableAPPProject {
     private static SQLiteConnection sQLiteConnection;
     public static void main(String args[]) {
         String tblPrefix = "tbl_";
+        String sqlQuery = "";
         sQLiteConnection = SQLiteConnection.getInstance("sqlite/create.sqlite3");
-        String sqlQuery = "CREATE TABLE IF NOT EXISTS " + tblPrefix + "app_api_project ("
+        Connection conn = sQLiteConnection.onOpenConnection();
+        sqlQuery = "DROP TABLE IF EXISTS " + tblPrefix + "app_api_project";
+        sQLiteConnection.onSqlExecute(sqlQuery);
+        sqlQuery = "CREATE TABLE IF NOT EXISTS " + tblPrefix + "app_api_project ("
                 + " taap_project_id BIGINT(20) NOT NULL,"
                 + " taap_project_modified_by BIGINT(20) NULL,"
                 + " CONSTRAINT pk_app_api_project_taap_project_id PRIMARY KEY (taap_project_id)"
                 + ");";
-        Connection conn = sQLiteConnection.onOpenConnection();
         sQLiteConnection.onSqlExecute(sqlQuery);
         sQLiteConnection.onClose();
         System.out.println("ID# " + RandomValue.getRandId(1111, 9999));
@@ -46,4 +49,5 @@ try {
     if (c != null)
         c.close();
 }
+String query = String.format("SELECT %s FROM %s LIMIT 1", column, table);
 */
